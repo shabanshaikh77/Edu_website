768c71766d562424aec7aa4a918842104cfa8eab
unknown <lcs2020044@iiitl.ac.in>
2025-04-16 02:05:33 +0530

    bug fixed

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const connectDB = require('./utils/db');
 const errorMiddleware = require('./middleware/error-middleware');
 
 
-app.use(cors);  
+app.use(cors());  
 app.use(express.json());
 
 app.use("/api/auth", authRoute); 
